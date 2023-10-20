3e158402e0be2d82291b9414fa9457ce55693dac
chris <chrismotari@gmail.com>
2023-10-20 15:01:02 +0300

    New build

--- src/axios_service.js
+++ src/axios_service.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
 axios.defaults.withCredentials=true
-axios.defaults.baseURL = import.meta.env.VUE_APP_BASE_API_URL
+// axios.defaults.baseURL = import.meta.env.VUE_APP_BASE_API_URL
+axios.defaults.baseURL = "http://localhost:8000"
 // axios.defaults.headers.common['ACCEPT'] = 'application/json';
