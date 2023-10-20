74c1fdabe84985d306c32999ecd6bb640bdb599b
chris <chrismotari@gmail.com>
2023-10-20 15:35:15 +0300

    Vite process config

--- src/axios_service.js
+++ src/axios_service.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 axios.defaults.withCredentials=true
-// axios.defaults.baseURL = import.meta.env.VUE_APP_BASE_API_URL
+axios.defaults.baseURL = process.env.VUE_APP_BASE_API_URL
 // axios.defaults.baseURL = "https://careees-api.smartstaffingsupport.com"
-axios.defaults.baseURL = "http://localhost:8000"
+// axios.defaults.baseURL = "http://localhost:8000"
 // axios.defaults.headers.common['ACCEPT'] = 'application/json';
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  css: {
-    devSourcemap: true,
-  },
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return{
+    define: { "process.env": env },
+    css: {
+      devSourcemap: true,
+    },
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
