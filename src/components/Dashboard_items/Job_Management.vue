c04a1d476a23369f99851916833ea195e0b0e93d
Ruel Montalbo Jr <rmontalbojr@611maa.local>
2023-10-26 15:18:27 -0700

    search working

--- src/components/Dashboard_items/Job_Management.vue
+++ src/components/Dashboard_items/Job_Management.vue
@@ -163,7 +163,13 @@
               </button>
             </div>
             <div class="w-50 d-flex">
-             <input class="form-control mr-sm-2" type="search" placeholder="Search Job" aria-label="Search" >
+              <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search Job"
+                aria-label="Search"
+                v-model="searchQuery"
+              />
             </div>
           </div>
         </div>
@@ -195,7 +201,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(item, index) in baseData" :key="index">
+            <tr v-for="(item, index) in filteredData" :key="index">
               <td>
                 <input
                   class="form-check-input"
@@ -210,6 +216,7 @@
               <td>{{ item.location }}</td>
               <td>
                 <div class="flex d-flex justify-content-center gap-2">
+                  <img :src="edit_icon" alt="edit_icon" />
                   <img :src="info" alt="Info Icon" />
                   <img :src="check" alt="Accept Icon" />
                   <img :src="Delete_icon" alt="Delete Icon" />
@@ -218,15 +225,61 @@
             </tr>
           </tbody>
         </table>
+        <div class="d-flex gap-4">
+          <div class="w-50 d-flex gap-4">
+            <div class="Status_pages">
+              Page {{ currentPage }} out of {{ totalPages }}
+            </div>
+            <div class="Status_pages">
+              <button class="View_all_btn">
+                <u><b>View All</b></u>
+              </button>
+            </div>
+          </div>
+          <div class="w-50 d-flex justify-content-end gap-4">
+            <select v-model="currentPage">
+              <option
+                v-for="page in totalPageOptions"
+                :key="page"
+                :value="page"
+              >
+                {{ page }}
+              </option>
+            </select>
+
+            <button
+              @click="previousPage"
+              :disabled="currentPage === 1"
+              class="NoBgNoBor"
+            >
+              <img :src="Previous_icon" alt="Previous_icon" />
+            </button>
+
+            <!-- Next Button -->
+            <button
+              class="NoBgNoBor"
+              @click="nextPage"
+              :disabled="currentPage === totalPages"
+            >
+              <img :src="Next_icon" alt="Next_icon" />
+            </button>
+          </div>
+
+          <!-- Page Selector Dropdown -->
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script setup>
+import Next_icon from "@/assets/Images/table_icons/Next.svg";
+import Previous_icon from "@/assets/Images/table_icons/previous.svg";
+
 import Delete_icon from "@/assets/Images/delete.png";
 import check from "@/assets/Images/check.png";
 import info from "@/assets/Images/info.png";
+import edit_icon from "@/assets/Images/action_icon/edit_icon_action.svg";
 
 import sort_icon from "@/assets/Images/table_icons/sort-solid 1.svg";
 
@@ -236,11 +289,53 @@ import phil_flag from "@/assets/Images/flag_only/flag philippines.png";
 import kenya_flag from "@/assets/Images/flag_only/kenya_flag.png";
 import lebanon_flag from "@/assets/Images/flag_only/lebanon_flag.png";
 
-import { ref } from "vue";
+import { ref, computed } from "vue";
+
+const searchQuery = ref("");
 
 const rangeValue = ref(1000);
 
+const filteredData = computed(() => {
+  const query = searchQuery.value.toLowerCase().trim();
+  if (!query) {
+    return baseData.value;
+  }
+
+  return baseData.value.filter((item) => {
+    const jobTitle = item.job_title.toLowerCase();
+    return jobTitle.includes(query);
+  });
+});
+
 const baseData = ref([
+  {
+    select: "Select",
+    job_title: "Customer Service Representative",
+    job_category: "Full-Time",
+    salary: "$25,000",
+    location: "USA",
+  },
+  {
+    select: "Select",
+    job_title: "Technical Support Specialist",
+    job_category: "Part-Time",
+    salary: "$20,000",
+    location: "Philippines",
+  },
+  {
+    select: "Select",
+    job_title: "Quality Assurance Analyst",
+    job_category: "Remote",
+    salary: "$30,000",
+    location: "Kenya",
+  },
+  {
+    select: "Select",
+    job_title: "Team Leader - BPO Operations",
+    job_category: "Full-Time",
+    salary: "$35,000",
+    location: "Lebanon",
+  },
   {
     select: "Select",
     job_title: "Customer Service Representative",
@@ -275,6 +370,11 @@ const paginatedData = ref(baseData);
 </script>
 
 <style>
+.View_all_btn {
+  background: none;
+  border-style: none;
+}
+
 .Delete_Selected_btn {
   color: white;
   font-size: 18px;
@@ -299,6 +399,7 @@ const paginatedData = ref(baseData);
 
 .tableJob_hold {
   padding: 15px 25px;
+  border-radius: 15px;
 }
 
 .salary_range_hold,
