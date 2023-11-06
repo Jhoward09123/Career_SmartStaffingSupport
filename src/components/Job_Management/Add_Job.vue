fbd418546e4ce9a1b0f8594f65c2354fe0cd6cb3
chris <chrismotari@gmail.com>
2023-11-06 17:41:46 +0300

    Add job

--- src/components/Job_Management/Add_Job.vue
+++ src/components/Job_Management/Add_Job.vue
@@ -11,26 +11,22 @@
       <div class="mt-4">Create Job Form</div>
 
       <div class="border_add_box border border-secondary">
-        <form @submit.prevent="store.handleAddJob(form_data)">
+        <form @submit.prevent="store.createJob(form_data)">
           <div class="col_hold_3 d-flex p-4">
             <div class="column_3 px-2">
               <div class="">
                 <div class="Label_title">Job Title</div>
                 <div class="Label_title">
-                  <input type="text" class="input_one px-2" />
+                  <input type="text" class="input_one px-2" v-model="form_data.name" name="name"/>
                 </div>
               </div>
               <div class="item_hold d-flex gap-3">
                 <div class="left_cate w-50">
                   <div class="Label_title">Job Category</div>
                   <div class="Label_title">
-                    <select
-                      class="form-select two_input"
-                      aria-label="Default select example"
-                    >
-                      <option selected>Hybrid</option>
-                      <option value="2">On site</option>
-                      <option value="3">Remote</option>
+                    <select class="form-select two_input" aria-label="job_category_id" v-model="form_data.job_category_id" name="job_category_id">
+                      <option v-for="job_category in state.job_categories" :value="job_category.id">{{ job_category.name }}</option>
+
                     </select>
                   </div>
                 </div>
@@ -40,10 +36,10 @@
                     <select
                       class="form-select two_input"
                       aria-label="Default select example"
+                      v-model="form_data.contract_type_id" name="contract_type_id"
                     >
-                      <option selected>Full-time</option>
-                      <option value="2">Part-Time</option>
-                      <option value="3">Remote</option>
+                      <option v-for="contract_type in state.contract_types" :value="contract_type.id">{{ contract_type.name }}</option>
+
                     </select>
                   </div>
                 </div>
@@ -55,18 +51,17 @@
                     <select
                       class="form-select two_input"
                       aria-label="Default select example"
+                      v-model="form_data.country_id" name="country_id"
                     >
-                      <option value="AF">USA</option>
-                      <option value="AX">KENYA</option>
-                      <option value="AL">PHILIPPINES</option>
-                      <option value="DZ">LEBANON</option>
+                      <option v-for="country in state.countries" :value="country.id">{{ country.name }}</option>
+
                     </select>
                   </div>
                 </div>
                 <div class="right_con w-50">
                   <div class="Label_title">City</div>
                   <div class="Label_title">
-                    <input type="text" class="two_input" />
+                    <input type="text" class="two_input" v-model="form_data.city" name="city"/>
                   </div>
                 </div>
               </div>
@@ -75,13 +70,13 @@
                 <div class="d-flex gap-3">
                   <div class="left_cate w-50">
                     <div class="Label_title">
-                      <input type="number" class="two_input" />
+                      <input type="number" class="two_input" v-model="form_data.min_salary" name="min_salary"/>
                     </div>
                   </div>
                   <div class="right_con w-50">
                     <div class="Label_title"></div>
                     <div class="Label_title">
-                      <input type="number" class="two_input" />
+                      <input type="number" class="two_input" v-model="form_data.max_salary" name="max_salary"/>
                     </div>
                   </div>
                 </div>
@@ -93,10 +88,9 @@
                     <select
                       class="form-select two_input"
                       aria-label="Default select example"
+                      v-model="form_data.company_id" name="company_id"
                     >
-                      <option selected>SSS</option>
-                      <option value="2">ULG</option>
-                      <option value="3">611</option>
+                      <option v-for="company in state.companies" :value="company.id">{{ company.name }}</option>
                     </select>
                   </div>
                 </div>
@@ -107,6 +101,8 @@
                       class="form-control form-control-lg"
                       id="formFileLg"
                       type="file"
+                      name="flyer_url"
+                      v-bind="form_data.flyer_url"
                     />
                   </div>
                 </div>
@@ -124,6 +120,7 @@
                       placeholder="Leave a comment here"
                       id="floatingTextarea2"
                       style="height: 100px"
+                      v-model="form_data.about_job" name="about_job"
                     ></textarea>
                   </div>
                 </div>
@@ -147,7 +144,7 @@
                         <button
                           @click="addDescription"
                           class="btn-light add_btn_description"
-                          type="submit"
+                          type="button"
                         >
                           <img :src="add_btn_des" alt="add_btn_des" />
                         </button>
@@ -157,9 +154,9 @@
                       <div class="overflow-auto m-2">
                         <ul class="list_job_descript">
                           <li class="border_item_list">
-                            <div v-if="descriptions.length > 0">
+                            <div v-if="form_data.job_description.length > 0">
                               <div
-                                v-for="(description, index) in descriptions"
+                                v-for="(description, index) in form_data.job_description"
                                 :key="index"
                                 class="d-flex w-100 border_per_item"
                               >
@@ -169,7 +166,8 @@
                                 <div class="button_remove_hold">
                                   <button
                                     class="remove_btn"
-                                    @click="removeDescription"
+                                    @click="removeDescription(index)"
+                                    type="button"
                                   >
                                     <img
                                       :src="remove_list_icon"
@@ -200,7 +198,7 @@
                     <div class="d-flex gap-2">
                       <div class="">
                         <textarea
-                          v-model="newqualifications"
+                          v-model="newqualification"
                           class="form-control border_textarea"
                           placeholder=""
                           id="floatingTextarea2"
@@ -211,7 +209,7 @@
                         <button
                           @click="addqualifications"
                           class="btn-light add_btn_description"
-                          type="submit"
+                          type="button"
                         >
                           <img :src="add_btn_des" alt="add_btn_des" />
                         </button>
@@ -221,9 +219,9 @@
                       <div class="overflow-auto m-2">
                         <ul class="list_job_descript">
                           <li class="border_item_list">
-                            <div v-if="qualifications.length > 0">
+                            <div v-if="form_data.qualifications.length > 0">
                               <div
-                                v-for="(qualification, index) in qualifications"
+                                v-for="(qualification, index) in form_data.qualifications"
                                 :key="index"
                                 class="d-flex w-100 border_per_item"
                               >
@@ -233,7 +231,8 @@
                                 <div class="button_remove_hold">
                                   <button
                                     class="remove_btn"
-                                    @click="removeQualifications"
+                                    @click="removeQualification(index)"
+                                    type="button"
                                   >
                                     <img
                                       :src="remove_list_icon"
@@ -257,7 +256,7 @@
               </div>
               <hr />
               <div class="flex d-flex justify-content-center btn_submit_hold">
-                <button class="btn-primary Submit_btn_add">Sumbit</button>
+                <button class="btn-primary Submit_btn_add" type="submit">Submit</button>
               </div>
             </div>
           </div>
@@ -272,38 +271,80 @@
 import remove_list_icon from "@/assets/Images/action_icon/remove_list_icon.svg";
 import add_btn_des from "@/assets/Images/Btn_icons/add_btn_descr.svg";
 import Side_panel from "@/components/Side_panel.vue";
+import { useGlobalStore } from "@/stores/globalStore";
+import { ref,onMounted } from "vue";
 
-import { ref } from "vue";
+const store = useGlobalStore();
 
-const descriptions = ref([]);
+const state = ref({
+  "companies" : [],
+  "states" : [],
+  "job_categories" : [],
+  "countries" : [],
+  "contract_types" : []
+})
+
+onMounted( async () => {
+  await store.fetchCompanies()
+  await store.fetchStates()
+  await store.fetchJobCategories()
+  await store.fetchCountries()
+  await store.fetchContractTypes()
+  state.value.companies = store.companies
+  state.value.states = store.states
+  state.value.job_categories = store.job_categories
+  state.value.countries = store.countries
+  state.value.contract_types = store.contract_types
+  
+})
+
+const form_data = ref({
+  "job_category_id": "",
+  "profession_id": "",
+  "contract_type_id": "",
+  "company_id": "",
+  "country_id": "",
+  "city": "",
+  "name": "",
+  "min_salary": "",
+  "max_salary": "",
+  "about_job": "",
+  "responsibilities": "",
+  "job_description": [],
+  "qualifications": [],
+  "tags": "",
+  "application_deadline": "",
+  "flyer_url": ""
+})
+
+
+// const descriptions = ref([]);
+// const qualifications = ref([]);
+const newqualification = ref("");
 const newDescription = ref("");
 const currencyValue = ref("");
 
 const addDescription = () => {
   if (newDescription.value) {
-    descriptions.value.push(newDescription.value);
+    form_data.value.job_description.push(newDescription.value);
     newDescription.value = ""; // Clear the input field
   }
-};
+}; 
 
 //new qualifications
-
-const qualifications = ref([]);
-const newqualifications = ref("");
-
 const addqualifications = () => {
-  if (newqualifications.value) {
-    qualifications.value.push(newqualifications.value);
-    newqualifications.value = ""; // Clear the input field
+  if (newqualification.value) {
+    form_data.value.qualifications.push(newqualification.value);
+    newqualification.value = ""; // Clear the input field
   }
 };
 
 const removeDescription = (index) => {
-  descriptions.value.splice(index, 1);
+  form_data.value.job_description.splice(index, 1);
 };
 
-const removeQualifications = (index) => {
-  qualifications.value.splice(index, 1);
+const removeQualification = (index) => {
+  form_data.value.qualifications.splice(index, 1); 
 };
 
 //currency
--- src/stores/globalStore.js
+++ src/stores/globalStore.js
@@ -7,7 +7,16 @@ export const useGlobalStore = defineStore('global', () => {
     const state = ref({
         jobs: null
     })
+
     const jobs = ref([])
+    const users = ref([])
+    const companies = ref([])
+    const states = ref([])
+    const countries = ref([])
+    const job_categories = ref([])
+    const contract_types = ref([])
+    // const companies = ref([])
+
     async function  getToken() {
         await axios.get("/sanctum/csrf-cookie")
     }
@@ -25,6 +34,51 @@ export const useGlobalStore = defineStore('global', () => {
             return res.data.data
         })  
     }
+    async function fetchCompanies(){
+        await getToken()
+        await axios.get('/api/companies').then((res) => {
+            console.log(res.data.data)
+            companies.value = res.data.data
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+    async function fetchCountries(){
+        await getToken()
+        await axios.get('/api/countries').then((res) => {
+            console.log(res.data.data)
+            countries.value = res.data.data
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+    async function fetchStates(){
+        await getToken()
+        await axios.get('/api/states').then((res) => {
+            console.log(res.data.data)
+            states.value = res.data.data
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+    async function fetchJobCategories(){
+        await getToken()
+        await axios.get('/api/job-categories').then((res) => {
+            console.log(res.data.data)
+            job_categories.value = res.data.data
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+    async function fetchContractTypes(){
+        await getToken()
+        await axios.get('/api/contract-types').then((res) => {
+            console.log(res.data.data)
+            contract_types.value = res.data.data
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
 
     async function getJobCategory(id) {
         await getToken()
@@ -41,12 +95,37 @@ export const useGlobalStore = defineStore('global', () => {
     }
     async function createJob(form_data) {
         await getToken()
-        console.log(form_data)
-        return form_data
-        const res = await axios.get('api/countries/'+id)
-        jobs.value = res.data.data
+        // console.log(form_data)
+        await axios.post('/api/jobs',form_data).then((res) => {
+            console.log(res.data)
+            getJobs()
+            // this.router.push({name: ''})
+        }).catch((error) => {
+            console.log(error.response.data)
+        })
+
         
     }
   
-    return { getJobs, state,jobs,getJobCategory, getJob, getCountry }
+    return { 
+        state,
+        jobs,
+        companies,
+        users,
+        states,
+        countries,
+        job_categories,
+        contract_types,
+        getJobs, 
+        getJobCategory, 
+        getJob, 
+        getCountry, 
+        createJob,
+    
+        fetchCompanies,
+        fetchStates,
+        fetchContractTypes,
+        fetchCountries,
+        fetchJobCategories
+    }
 })
